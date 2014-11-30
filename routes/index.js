53dc8358a48e0755a8980762befab52f5d55d70e
dellagustin <dellagustin@gmail.com>
2014-11-30 17:31:15 +0100

    Initial Commit   The current version is able to list the episodes in the starting page and show individual episodes.   Integration with disqus is implemented for the episode pages.

---
+++ routes/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/episode/:episodeNumber', function(req, res) {
+	
+	var episodeNumber = req.params.episodeNumber;
+	
+	var content = require('../content/content.json');
+	
+	var episode;
+	
+	content.episodes.forEach(function(item) {
+	    if(item.number === episodeNumber) {
+	    	episode = item;
+	    }
+	});
+	
+	if(episode) {
+		episode.downloadLink = 'http://dstats.net/download/http://www6.ufrgs.br/frontdaciencia/arquivos/' + episode.audioFile;
+		episode.imageUrl = episode.imageFile ? 'http://www.ufrgs.br/frontdaciencia/imagens/' + episode.imageFile : false;
+		episode.disqusID = '\'Episode_' + episode.number + '\'';
+		
+		res.render('episode', { episode: episode });
+	}
+	else {
+		res.render('episode_not_found', { episode: episode });
+    }
+});
+
+/* GET home page. */
+router.get('/', function(req, res) {
+	var content = require('../content/content.json');
+	
+	content.episodes.sort( function(a, b) {
+		return b.number-a.number;
+	});
+	
+	content.episodes.forEach(function(item) {
+		item.imageUrl = item.imageFile ? 'http://www.ufrgs.br/frontdaciencia/imagens/' + item.imageFile : false;
+		item.linkToArticle = 'episode/' + item.number;
+	});
+	
+	console.log(content.episodes);
+	
+	res.render('index', { episodes: content.episodes});
+});
+
+module.exports = router;
