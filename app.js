10ec4981f486f9eb71fbe44d7c71f125d52ec6e9
dellagustin <dellagustin@gmail.com>
2014-12-09 23:09:49 +0100

    Created support for seasons and season links in the layout

--- app.js
+++ app.js
@@ -36,6 +36,9 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
+        
+        console.log(err);
+        
         res.render('error', {
             message: err.message,
             error: err
@@ -47,6 +50,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
+    
     res.render('error', {
         message: err.message,
         error: {}
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
+function prepareEpisode(episode) {
+	episode.imageUrl = episode.imageFile ? 'http://www.ufrgs.br/frontdaciencia/imagens/' + episode.imageFile : false;
+	episode.linkToArticle = '/episode/' + episode.number;
+	episode.downloadCounterLink = 'http://dstats.net/dstatsjs.php?file=http://www6.ufrgs.br/frontdaciencia/arquivos/' + episode.audioFile;
+	episode.disqusID = '\'Episode_' + episode.number + '\'';
+}
+
+function prepareEpisodes(episodes) {
+	episodes.forEach(prepareEpisode);
+}
+
+function getContent() {
+	var content = require('../content/content.json');
+	
+	return content;
+}
+
+router.get('/season/:seasonNumber', function(req, res) {
+	
+	var seasonNumber = req.params.seasonNumber;
+	var seasonEspisodes = [];
+	var content = getContent();
+	
+	content.episodes.sort( function(a, b) {
+		return b.number-a.number;
+	});
+	
+	content.episodes.forEach(function(item) {
+		if(parseInt(item.seasonNumber) === parseInt(seasonNumber)) {
+			seasonEspisodes.push(item);
+		}
+	});
+	
+	prepareEpisodes(seasonEspisodes);
+		
+	res.render('index', { episodes: seasonEspisodes});
+});
+
 router.get('/episode/:episodeNumber', function(req, res) {
 	
 	var episodeNumber = req.params.episodeNumber;
 	
-	var content = require('../content/content.json');
+	var content = getContent();
 	
 	var episode;
 	
@@ -16,11 +54,7 @@ router.get('/episode/:episodeNumber', function(req, res) {
 	});
 	
 	if(episode) {
-		episode.downloadLink = 'http://dstats.net/download/http://www6.ufrgs.br/frontdaciencia/arquivos/' + episode.audioFile;
-		episode.imageUrl = episode.imageFile ? 'http://www.ufrgs.br/frontdaciencia/imagens/' + episode.imageFile : false;
-		episode.downloadCounterLink = 'http://dstats.net/dstatsjs.php?file=http://www6.ufrgs.br/frontdaciencia/arquivos/' + episode.audioFile;
-		episode.disqusID = '\'Episode_' + episode.number + '\'';
-		
+		prepareEpisode(episode);
 		res.render('episode', { episode: episode });
 	}
 	else {
@@ -31,20 +65,13 @@ router.get('/episode/:episodeNumber', function(req, res) {
 /* GET home page. */
 router.get('/', function(req, res) {
 
-	var content = require('../content/content.json');
+	var content = getContent();
 	
 	content.episodes.sort( function(a, b) {
 		return b.number-a.number;
 	});
 	
-	content.episodes.forEach(function(item) {
-		item.imageUrl = item.imageFile ? 'http://www.ufrgs.br/frontdaciencia/imagens/' + item.imageFile : false;
-		item.linkToArticle = 'episode/' + item.number;
-		item.downloadCounterLink = 'http://dstats.net/dstatsjs.php?file=http://www6.ufrgs.br/frontdaciencia/arquivos/' + item.audioFile;
-		item.disqusID = '\'Episode_' + item.number + '\'';
-	});
-	
-	console.log(content.episodes);
+	prepareEpisodes(content.episodes);
 	
 	res.render('index', { episodes: content.episodes});
 });
